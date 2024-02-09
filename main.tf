8497c0857a9556fdad7bc30cbbff396817c39478
Rafael Carrau <rafaelcarrau@Rafaels-MacBook-Air.local>
2024-02-08 21:14:54 -0600

    Changes

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ provider "aws" {
 }
 
 variable "cluster_name" {
-  default = "g5-capstone2-eks-cluster"
+  default = "gr5-capstone2-eks-cluster"
 }
 
 variable "cluster_version" {
   default = "1.28"
 }
 
-resource "aws_vpc" "g5_capstone2_vpc_stack" {
+resource "aws_vpc" "gr5_capstone2_vpc_stack" {
   cidr_block = "192.168.0.0/16"
 
 
@@ -27,139 +27,139 @@ resource "aws_vpc" "g5_capstone2_vpc_stack" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "g5-capstone2-vpc-stack"
+    Name = "gr5-capstone2-vpc-stack"
   }
 }
 
-resource "aws_internet_gateway" "g5_capstone2_igw" {
-  vpc_id = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_internet_gateway" "gr5_capstone2_igw" {
+  vpc_id = aws_vpc.gr5_capstone2_vpc_stack.id
 
   tags = {
-    Name = "g5-capstone2-igw"
+    Name = "gr5-capstone2-igw"
   }
 }
 
-resource "aws_subnet" "g5_capstone2_private_us_west_2a" {
-  vpc_id            = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_subnet" "gr5_capstone2_private_us_west_2a" {
+  vpc_id            = aws_vpc.gr5_capstone2_vpc_stack.id
   cidr_block        = "192.168.128.0/18"
   availability_zone = "us-west-2a"
 
   tags = {
-    "Name"                                      = "g5-capstone2-private-us-west-2a"
+    "Name"                                      = "gr5-capstone2-private-us-west-2a"
     "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
-resource "aws_subnet" "g5_capstone2_private_us_west_2b" {
-  vpc_id            = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_subnet" "gr5_capstone2_private_us_west_2b" {
+  vpc_id            = aws_vpc.gr5_capstone2_vpc_stack.id
   cidr_block        = "192.168.192.0/18"
   availability_zone = "us-west-2b"
 
   tags = {
-    "Name"                                      = "g5-capstone2-private-us-west-2b"
+    "Name"                                      = "gr5-capstone2-private-us-west-2b"
     "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
-resource "aws_subnet" "g5_capstone2_public_us_west_2a" {
-  vpc_id            = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_subnet" "gr5_capstone2_public_us_west_2a" {
+  vpc_id            = aws_vpc.gr5_capstone2_vpc_stack.id
   cidr_block        = "192.168.0.0/18"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                                      = "g5-capstone2-private-us-west-2a"
+    "Name"                                      = "gr5-capstone2-private-us-west-2a"
     "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
-resource "aws_subnet" "g5_capstone2_public_us_west_2b" {
-  vpc_id            = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_subnet" "gr5_capstone2_public_us_west_2b" {
+  vpc_id            = aws_vpc.gr5_capstone2_vpc_stack.id
   cidr_block        = "192.168.64.0/18"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                                      = "g5-capstone2-private-us-west-2b"
+    "Name"                                      = "gr5-capstone2-private-us-west-2b"
     "kubernetes.io/role/internal-elb"           = "1"
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
 
-resource "aws_eip" "g5_capstone2_nat" {
+resource "aws_eip" "gr5_capstone2_nat" {
   vpc = true
 
   tags = {
-    Name = "g5-capstone2-nat"
+    Name = "gr5-capstone2-nat"
   }
 }
 
-resource "aws_nat_gateway" "g5_capstone2_nat" {
-  allocation_id = aws_eip.g5_capstone2_nat.id
-  subnet_id     = aws_subnet.g5_capstone2_public_us_west_2a.id
+resource "aws_nat_gateway" "gr5_capstone2_nat" {
+  allocation_id = aws_eip.gr5_capstone2_nat.id
+  subnet_id     = aws_subnet.gr5_capstone2_public_us_west_2a.id
 
   tags = {
-    Name = "g5-capstone2-nat"
+    Name = "gr5-capstone2-nat"
   }
 
-  depends_on = [aws_internet_gateway.g5_capstone2_igw]
+  depends_on = [aws_internet_gateway.gr5_capstone2_igw]
 }
 
-resource "aws_route_table" "g5_capstone2_private" {
-  vpc_id = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_route_table" "gr5_capstone2_private" {
+  vpc_id = aws_vpc.gr5_capstone2_vpc_stack.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.g5_capstone2_nat.id
+    nat_gateway_id = aws_nat_gateway.gr5_capstone2_nat.id
   }
 
   tags = {
-    Name = "g5-capstone2-private"
+    Name = "gr5-capstone2-private"
   }
 }
 
-resource "aws_route_table" "g5_capstone2_public" {
-  vpc_id = aws_vpc.g5_capstone2_vpc_stack.id
+resource "aws_route_table" "gr5_capstone2_public" {
+  vpc_id = aws_vpc.gr5_capstone2_vpc_stack.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.g5_capstone2_igw.id
+    gateway_id = aws_internet_gateway.gr5_capstone2_igw.id
   }
 
   tags = {
-    Name = "g5-capstone2-public"
+    Name = "gr5-capstone2-public"
   }
 }
 
-resource "aws_route_table_association" "g5_capstone2_private_us_west_2a" {
-  subnet_id      = aws_subnet.g5_capstone2_private_us_west_2a.id
-  route_table_id = aws_route_table.g5_capstone2_private.id
+resource "aws_route_table_association" "gr5_capstone2_private_us_west_2a" {
+  subnet_id      = aws_subnet.gr5_capstone2_private_us_west_2a.id
+  route_table_id = aws_route_table.gr5_capstone2_private.id
 }
 
-resource "aws_route_table_association" "g5_capstone2_private-us-west-2b" {
-  subnet_id      = aws_subnet.g5_capstone2_private_us_west_2b.id
-  route_table_id = aws_route_table.g5_capstone2_private.id
+resource "aws_route_table_association" "gr5_capstone2_private-us-west-2b" {
+  subnet_id      = aws_subnet.gr5_capstone2_private_us_west_2b.id
+  route_table_id = aws_route_table.gr5_capstone2_private.id
 }
 
-resource "aws_route_table_association" "g5_capstone2_public-us-west-2a" {
-  subnet_id      = aws_subnet.g5_capstone2_public_us_west_2a.id
-  route_table_id = aws_route_table.g5_capstone2_public.id
+resource "aws_route_table_association" "gr5_capstone2_public-us-west-2a" {
+  subnet_id      = aws_subnet.gr5_capstone2_public_us_west_2a.id
+  route_table_id = aws_route_table.gr5_capstone2_public.id
 }
 
-resource "aws_route_table_association" "g5_capstone2_public-us-west-2b" {
-  subnet_id      = aws_subnet.g5_capstone2_public_us_west_2b.id
-  route_table_id = aws_route_table.g5_capstone2_public.id
+resource "aws_route_table_association" "gr5_capstone2_public-us-west-2b" {
+  subnet_id      = aws_subnet.gr5_capstone2_public_us_west_2b.id
+  route_table_id = aws_route_table.gr5_capstone2_public.id
 }
 
-resource "aws_iam_role" "g5_capstone2_eks_cluster_role" {
-  name = "g5-capstone2-eks-cluster-role"
-  assume_role_policy = data.aws_iam_policy_document.g5_assume_role.json
+resource "aws_iam_role" "gr5_capstone2_eks_cluster_role" {
+  name = "gr5-capstone2-eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.gr5_assume_role.json
 }
 
-data "aws_iam_policy_document" "g5_assume_role" {
+data "aws_iam_policy_document" "gr5_assume_role" {
   statement {
     effect = "Allow"
 
@@ -171,15 +171,15 @@ data "aws_iam_policy_document" "g5_assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
-resource "aws_iam_role_policy_attachment" "g5_capstone2_cluster_policy" {
+resource "aws_iam_role_policy_attachment" "gr5_capstone2_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.g5_capstone2_eks_cluster_role.name}"
+  role       = "${aws_iam_role.gr5_capstone2_eks_cluster_role.name}"
 }
 
-resource "aws_eks_cluster" "g5_capstone2_eks_cluster" {
+resource "aws_eks_cluster" "gr5_capstone2_eks_cluster" {
   name     = var.cluster_name
   version  = var.cluster_version
-  role_arn = aws_iam_role.g5_capstone2_eks_cluster_role.arn
+  role_arn = aws_iam_role.gr5_capstone2_eks_cluster_role.arn
 
   vpc_config {
 
@@ -188,23 +188,23 @@ resource "aws_eks_cluster" "g5_capstone2_eks_cluster" {
     public_access_cidrs     = ["0.0.0.0/0"]
 
     subnet_ids = [
-      aws_subnet.g5_capstone2_private_us_west_2a.id,
-      aws_subnet.g5_capstone2_private_us_west_2b.id,
-      aws_subnet.g5_capstone2_public_us_west_2a.id,
-      aws_subnet.g5_capstone2_public_us_west_2b.id
+      aws_subnet.gr5_capstone2_private_us_west_2a.id,
+      aws_subnet.gr5_capstone2_private_us_west_2b.id,
+      aws_subnet.gr5_capstone2_public_us_west_2a.id,
+      aws_subnet.gr5_capstone2_public_us_west_2b.id
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.g5_capstone2_cluster_policy]
+  depends_on = [aws_iam_role_policy_attachment.gr5_capstone2_cluster_policy]
 
 }
 
-resource "aws_iam_role" "g5_capstone2_eks_fargate_profile" {
-  name = "g5-capstone2-eks-fargate-profile"
-  assume_role_policy = data.aws_iam_policy_document.g5_assume_role_fargate.json
+resource "aws_iam_role" "gr5_capstone2_eks_fargate_profile" {
+  name = "gr5-capstone2-eks-fargate-profile"
+  assume_role_policy = data.aws_iam_policy_document.gr5_assume_role_fargate.json
 }
 
-data "aws_iam_policy_document" "g5_assume_role_fargate" {
+data "aws_iam_policy_document" "gr5_assume_role_fargate" {
   statement {
     effect = "Allow"
 
@@ -217,7 +217,24 @@ data "aws_iam_policy_document" "g5_assume_role_fargate" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "g5_capstone2_eks-fargate-profile_policy" {
+resource "aws_iam_role_policy_attachment" "gr5_capstone2_eks-fargate-profile_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
-  role       = "${aws_iam_role.g5_capstone2_eks_fargate_profile.name}"
+  role       = "${aws_iam_role.gr5_capstone2_eks_fargate_profile.name}"
+}
+
+resource "aws_eks_fargate_profile" "gr5_capstone2_kube_system" {
+  cluster_name           = var.cluster_name
+  fargate_profile_name   = "gr5-capstone2-kube-system"
+  pod_execution_role_arn = aws_iam_role.gr5_capstone2_eks_fargate_profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.gr5_capstone2_private_us_west_2a.id,
+    aws_subnet.gr5_capstone2_private_us_west_2b.id
+  ]
+
+  selector {
+    namespace = "kube-system"
+  }
 }
